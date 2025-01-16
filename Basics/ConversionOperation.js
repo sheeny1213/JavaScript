82a592a8d390eda0529b6f9b6054b784ff089387
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-01-16 16:24:55 +0000

    Conversion of Datatypes

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,36 @@
+// let score = null
+// let score = undefined 
+// let score = true
+let score = "sandhya"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  "33" => 33
+// "33abc" => NaN
+// true  => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "sandhya"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "sandhya" => true 
+
+let someNumber = 21
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
