cb0ee177535f542a92d08b35f3f5deb4e339ec59
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-01-16 16:00:00 +0000

    Datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(8 + 9) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Sandhya")
+
+
+let name = "sandhya"
+let age = 18
+let isLoggedIn = false
+// let state; //Undefined
+let state = null // empty
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined = >
+//symbol => unique
+
+// object
+
+// console.log(typeof age);
+console.log(typeof null);
