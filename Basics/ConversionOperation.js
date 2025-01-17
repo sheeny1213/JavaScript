297e9a4327da4c47325e71c60939eb5db9f2f9a5
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-01-17 10:48:51 +0000

    ConversionOperation

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -30,7 +30,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 21
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ********************* Operations ************************
 
+let value = 2
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3); 
+
+let str1 = "hello"
+let str2 = " sandhya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true); // +true => 1 and true+ => error
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+
+let gameCounter = 100
+gameCounter++; // gameCounter++ => postfix incriment and ++gameCounter = > prefix Increment
+console.log(gameCounter);
+
+// Link to Study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
