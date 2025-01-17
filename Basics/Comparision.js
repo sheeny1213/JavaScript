22514115b1e68aae9951f9a734522792f370c2c7
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-01-17 11:30:16 +0000

    Comparision

---
+++ Basics/Comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+//in most of the cases we avoid this type of comparison
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === (strictly check also check datatypes )
+
+console.log("2" === 2);
+
