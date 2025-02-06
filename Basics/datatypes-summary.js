09dab6d6f3d48bafe59d9bb79cd95606f558f5d0
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-02-06 09:50:09 +0000

    Datatype-Summary

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 345678909876543n;
 
@@ -23,17 +23,43 @@ console.log(id === anotherId);
 const heros = ["shaktiman", "Naagraj", "doga"];
 let myObj =
 {
-    name: "hitesh",
+    name: "sandhya",
     age: 22,
 }
 
 const myFunction = function(){
     console.log("Hello World");
 }
-console.log(typeof bigNumber); //undefined
-console.log(typeof outsideTemp); //object
-console.log(typeof myFunction);//function
-console.log(typeof anotherId);//symbol
+// console.log(typeof bigNumber); //undefined
+// console.log(typeof outsideTemp); //object
+// console.log(typeof myFunction);//function
+// console.log(typeof anotherId);//symbol
 
-console.log(typeof heros); //object
+// console.log(typeof heros); //object
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  Stack (Primitive), Heap (Non-Primitive)
+
+let myGithubname = "Sheeny"
+
+let anothername = myGithubname
+anothername = "Sandhya"
+
+console.log(myGithubname); 
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sheeny@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
 
